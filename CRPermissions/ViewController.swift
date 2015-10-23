42237afb42b1baa64cf2062c25f41031eb820414
Cody Winton <cody@credntia.com>
2015-10-23 15:32:31 -0500

    Initial Commit

---
+++ CRPermissions/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CRPermissions
+//
+//  Created by Cody Winton on 10/23/15.
+//  Copyright © 2015 codeRed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
